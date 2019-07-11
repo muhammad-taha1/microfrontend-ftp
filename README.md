cbd36d861f6ae26bd9246e6b602453594d26d20a
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2019-07-11 02:47:57 -0400

    add 2nd script

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # microfrontend-ftp
+
+This project is used as a server for web components to be used by a background layer of a microfrontend app.
+
+## Running the service
+Run `node app.js` to run this service
+
+## API
+This project contains only 1 API:
+`/file/:filename`
+
+where filename has to be the filename we wish to retrieve from this project's directory. All web component js files have to be copied here and then served
--- app.js
+++ app.js
@@ -2,15 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/download', function(req, res){
-    const file = `./component-A.js`;
-    res.download(file); // Set disposition and send it.
-  });
-
-  app.get('/file', function(req, res){
-    const file = `/Angular/ftp/component-A.js`;
+  app.get('/file/:filename', function(req, res){
+    var fileName = req.params.filename;
+    const file = `/Angular/ftp/` + fileName;
+    console.log("requested filename: " + file);
     res.sendFile(file); // Set disposition and send it.
   });
 
